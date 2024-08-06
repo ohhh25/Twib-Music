e8c628eabd852b1fc682a84d05156750c5653b9b
Lukas Cao <lukascao06@gmail.com>
2024-08-06 13:36:36 -0700

    Update TrackView.swift

--- Twib Music/Views/TrackView.swift
+++ Twib Music/Views/TrackView.swift
@@ -11,12 +11,20 @@ struct TrackView: View {
     @StateObject var playlist: Playlist
     
     var body: some View {
+        HStack {
+            Spacer()
+            Text("\(playlist.tracks.count) songs")
+                .font(.custom("Helvetica", size: 16))
+                .italic()
+                .padding(.trailing, 24)
+        }
         List(playlist.tracks) { track in
             Text(track.name)
         }
         .onAppear() {
+            let url = playlist.tracks_url + "?limit=50"
             if playlist.tracks.isEmpty {
-                SpotifyAPI.fetchTracks(playlist)
+                SpotifyAPI.fetchTracks(playlist, url: url)
             }
         }
     }
