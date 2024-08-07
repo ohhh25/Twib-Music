3b2eeef78fed43713281ba33be79217a9d4fa2cf
Lukas Cao <lukascao06@gmail.com>
2024-08-07 00:55:47 -0700

    Update TrackView.swift

--- Twib Music/Views/TrackView.swift
+++ Twib Music/Views/TrackView.swift
@@ -18,8 +18,26 @@ struct TrackView: View {
                 .italic()
                 .padding(.trailing, 24)
         }
-        List(playlist.tracks) { track in
-            Text(track.name)
+        List(playlist.tracks) {track in
+            HStack {
+                AsyncImage(url: URL(string: track.image_url)) { image in
+                    image
+                        .resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                    .frame(width: 48, height: 48)
+                VStack(alignment: .leading) {
+                    Text(track.name)
+                        .font(.custom("Helvetica", size: 16))
+                        .padding(.bottom, 2)
+                    Text(track.artist)
+                        .font(.custom("Helvetica", size: 12))
+                        .fontWeight(.light)
+                }
+                .padding(.leading, 12)
+                Spacer()
+            }
         }
         .onAppear() {
             let url = playlist.tracks_url + "?limit=50"
