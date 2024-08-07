2a3e18d95cf48b69b4f6d55e1b52ed343f70bec0
Lukas Cao <lukascao06@gmail.com>
2024-08-07 00:55:43 -0700

    Song struct init

--- Twib Music/Models/Song.swift
+++ Twib Music/Models/Song.swift
@@ -9,20 +9,20 @@ import Foundation
 
 class Song: Identifiable {
     let name: String
-    let artist: String = ""
-    let others: [String] = []
-    let album: String = ""
-    let image_url: String = ""
-    let track_number: Int = 0
-    let duration: Int = 0
+    let artist: String
+    var others: [String] = []
+    let album: String
+    let image_url: String
+    let track_number: Int
+    let duration: Int
     
     // Help with matching
-    let sID: String = ""
-    let external_ids: [String: Any] = [:]
-    let preview_url: String = ""
-    let explicit: Int = -1
+    let sID: String
+    let external_ids: [String: Any]
+    let preview_url: String
+    let explicit: Int
     
-    let popularity: Int = -1    // fun
+    let popularity: Int
     
     init(name: String, artists: [[String: Any]],
          album: [String: Any], track_number: Int,
@@ -30,5 +30,20 @@ class Song: Identifiable {
          preview_url: String, explicit: Int, popularity: Int) {
         // Yeah, lots to do
         self.name  = name
+        self.artist = (artists[0]["name"] as? String) ?? ""
+        for artist in artists[1...] {
+            self.others.append(artist["name"] as? String ?? "")
+        }
+        self.album = album["name"] as? String ?? ""
+        let album_images = album["images"] as? [[String: Any]]
+        self.image_url = album_images?.suffix(1).first?["url"] as? String ?? ""
+        self.track_number = track_number
+        self.duration = duration
+        
+        self.sID = sID
+        self.external_ids = external_ids
+        self.preview_url = preview_url
+        self.explicit = explicit
+        self.popularity = popularity
     }
 }
