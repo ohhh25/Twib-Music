af55adbd97dd9a7eb843be6434896db9e6425ff6
Lukas Cao <lukascao06@gmail.com>
2024-08-09 15:30:00 -0700

    Create AlbumView.swift

---
+++ Twib Music/Views/AlbumView.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumView.swift
+//  Twib Music
+//
+//  Created by Lukas Cao on 8/9/24.
+//
+
+import SwiftUI
+
+struct AlbumView: View {
+    var album: Album
+    
+    var body: some View {
+        Text(album.name)
+    }
+}
