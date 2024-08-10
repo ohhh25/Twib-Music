0b03392e37ed80430c5bdabfe0a943e0fb5b7611
Lukas Cao <lukascao06@gmail.com>
2024-08-10 14:04:24 -0700

    Update Album.swift

--- Twib Music/Models/Album.swift
+++ Twib Music/Models/Album.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class Album: Playlist {
-   
+    let artist: String
+    var others: [String] = []
+    let type: String
+    let total_tracks: Int
+    let release_date: String
+    let external_ids: [String: Any]
+    let popularity: Int
+    
+    init(name: String, tracks_url: String, image_url: String,
+         artists: [[String: Any]], type: String, total_tracks: Int,
+         release_date: String, external_ids: [String: Any], popularity: Int) {
+        // first subclass init
+        self.artist = (artists[0]["name"] as? String) ?? ""
+        for artist in artists[1...] {
+            self.others.append(artist["name"] as? String ?? "")
+        }
+        self.type = type
+        self.total_tracks = total_tracks
+        self.release_date = release_date
+        self.external_ids = external_ids
+        self.popularity = popularity
+        super.init(name: name, tracks_url: tracks_url, image_url: image_url)
+    }
 }
