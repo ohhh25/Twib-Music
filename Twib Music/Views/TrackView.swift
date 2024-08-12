d58835f87f1c05a46ffbbc2e36e5a74d7ef09d2c
Lukas Cao <lukascao06@gmail.com>
2024-08-12 15:16:57 -0700

    Update TrackView.swift

--- Twib Music/Views/TrackView.swift
+++ Twib Music/Views/TrackView.swift
@@ -51,8 +51,9 @@ struct TrackView: View {
                     Text("\(idx + 1)")
                         .font(.custom("Helvetica", size: 16))
                         .frame(width: 18, alignment: .trailing)
+                        .padding(.leading, -6)
                 }
-                else { ImageView(image_url: track.image_url, size: 48) }
+                else { ImageView(image_url: track.image_url, size: 48).padding(.leading, -6) }
                 VStack(alignment: .leading) {
                     Text(track.name)
                         .font(.custom("Helvetica", size: 16))
@@ -66,10 +67,12 @@ struct TrackView: View {
                 .padding(.leading, 12)
                 Spacer()
                 Menu{
-                    Button("djfk", action: someFunction)
+                    Button("YouTube", action: someFunction)
                 } label: {
-                    Label("", systemImage: "ellipsis")
+                    Label("", systemImage: "ellipsis.circle")
+                        .font(.custom("Helvetica", size: 18))
                 }
+                .padding(EdgeInsets(top: 0, leading: 2, bottom: 0, trailing: -18))
             }
         }
         .onAppear() {
