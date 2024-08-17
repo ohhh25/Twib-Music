b4dbfec244e0d125b20407c0c8c4b1b23bbaf732
Lukas Cao <lukascao06@gmail.com>
2024-08-17 10:56:04 -0700

    Update server.mjs

--- TwibServer/server.mjs
+++ TwibServer/server.mjs
@@ -1,30 +1,27 @@
-import { createServer } from "node:http";
+import express from "express";
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const gitHub = "https://github.com/ohhh25/Twib-Music";
 
-const server = createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  if (req.url === "/") {
-    res.statusCode = 200;
-    res.end("<h1>Hello and Welcome to Twib Server!</h1>" +
-      "<p>Click <a href='/Twib-Music'>here</a> to visit Twib Music</p>"
-    );
-  } else if (req.url === "/Twib-Music") {
-    res.statusCode = 200;
-    // hyper-link to the github repo
-    res.end("<h1>Welcome to Twib Music!</h1>Check out the GitHub repo " +
-      `<a href="${gitHub}">here</a>`);
-  }
-  else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Page Not Found!\n');
-  }
+var app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Hello and Welcome to Twib Server!</h1>" +
+    "<p>Click <a href='/Twib-Music'>here</a> to visit Twib Music</p>"
+  );
 });
 
-server.listen(port, hostname, () => {
+app.get("/Twib-Music", (req, res) => {
+  res.status(200).send("<h1>Welcome to Twib Music!</h1>Check out the GitHub repo " +
+    `<a href="${gitHub}">here</a>`);
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send('Page Not Found!\n');
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
