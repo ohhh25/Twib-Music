9cc4ab0849f1a85ee4df5aad7310a1fbd6fd1ea2
Lukas Cao <lukascao06@gmail.com>
2024-08-17 17:20:17 -0700

    Update TrackView.swift

---
+++ Twib Music/Networking/TwibServerAPI.swift
@@ -0,0 +1,38 @@
+//
+//  TwibServerAPI.swift
+//  Twib Music
+//
+//  Created by Lukas Cao on 8/17/24.
+//
+
+import Foundation
+
+let hostname = "192.168.86.46"
+let port = 3000
+
+var TwibServerAPI = MyTwibServerAPI(hostname: hostname, port: port)
+
+class MyTwibServerAPI {
+    private let base: String
+    
+    init(hostname: String, port: Int) {
+        self.base = "http://\(hostname):\(port)/api/Twib-Music"
+    }
+    
+    func isrcSearch(_ isrc: String, completion: @escaping (String?) -> Void) {
+        guard let url = URL(string: self.base + "?isrc=" + isrc) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        SpotifyAPI.satisfyRequest(request) { data in
+            guard let data = data else { completion(nil); return }
+            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
+                completion(json["youtube"])
+                return
+            }
+            else {
+                print("Failed to parse the data in JSON")
+            }
+            completion(nil)
+        }
+    }
+}
