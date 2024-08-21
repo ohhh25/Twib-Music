8bcffe116c92a13447e7ba33fb3dd1d4509bc3f3
Lukas Cao <lukascao06@gmail.com>
2024-08-21 14:21:21 -0700

    Update TwibServerAPI.swift

--- Twib Music/Networking/TwibServerAPI.swift
+++ Twib Music/Networking/TwibServerAPI.swift
@@ -6,52 +6,53 @@
 //
 
 import Foundation
+import Zip
 
-let hostname = "192.168.86.46"
+let hostname = "HOSTNAME"
 let port = 3000
 
 var TwibServerAPI = MyTwibServerAPI(hostname: hostname, port: port)
 
 class MyTwibServerAPI {
     private let base: String
+    private let url: URL
     
     init(hostname: String, port: Int) {
         self.base = "http://\(hostname):\(port)/api/Twib-Music"
+        self.url = URL(string: self.base)!
     }
     
-    func isrcSearch(_ isrc: String, completion: @escaping (String?) -> Void) {
-        guard let url = URL(string: self.base + "?isrc=" + isrc) else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        SpotifyAPI.satisfyRequest(request) { data in
-            guard let data = data else { completion(nil); return }
-            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
-                completion(json["youtube"])
-                return
-            }
-            else {
-                print("Failed to parse the data in JSON")
-            }
-            completion(nil)
-        }
-    }
-    
-    func downloadPlaylist(_ jsonData: Data, completion: @escaping ([String]?) -> Void) {
-        guard let url = URL(string: self.base) else { return }
-        var request = URLRequest(url: url)
+    func prepareDownloadRequest(_ jsonData: Data) -> URLRequest {
+        var request = URLRequest(url: self.url)
         request.httpMethod = "POST"
         request.allHTTPHeaderFields = ["Content-Type": "application/json"]
         request.httpBody = jsonData
-        SpotifyAPI.satisfyRequest(request) { data in
-            guard let data = data else { return }
-            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: [String]] {
-                completion(json["youtube"])
+        return request
+    }
+    
+    func downloadPlaylist(_ jsonData: Data, completion: @escaping ([URL]?) -> Void) {
+        let request = prepareDownloadRequest(jsonData)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error downloading data: \(error?.localizedDescription ?? "Unknown error")")
+                completion(nil)
                 return
             }
-            else {
-                print("Failed to parse the data in JSON")
+            let zipFileURL = StorageManager.zipsDirectoryURL.appendingPathComponent("songs.zip")
+            print("Zip file URL: \(zipFileURL)")
+            do {
+                try data.write(to: zipFileURL)
+                try Zip.unzipFile(zipFileURL, destination: StorageManager.songsDirectoryURL, overwrite: true, password: nil)
+                let fileURLs = try StorageManager.manager.contentsOfDirectory(at: StorageManager.songsDirectoryURL, includingPropertiesForKeys: nil)
+                try StorageManager.manager.removeItem(at: zipFileURL)
+                completion(fileURLs)
+                return
+            } catch {
+                print("Error processing ZIP file: \(error.localizedDescription)")
+                completion(nil)
+                return
             }
-            completion(nil)
         }
+        task.resume()
     }
 }
