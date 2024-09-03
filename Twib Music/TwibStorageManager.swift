a17630384c76c2346fdf68a0af908145b03d49eb
Lukas Cao <lukascao06@gmail.com>
2024-09-03 11:52:27 -0700

    Update TwibStorageManager.swift
    Add Functionality for Clearing Cache/Download Directories and Syncing Directory Sizes

--- Twib Music/TwibStorageManager.swift
+++ Twib Music/TwibStorageManager.swift
@@ -10,12 +10,15 @@ import Zip
 
 var StorageManager = TwibStorageManager()
 
-class TwibStorageManager {
+class TwibStorageManager: ObservableObject {
     let manager = FileManager.default
     let tmpDirectoryURL = FileManager.default.temporaryDirectory
     let appSupportDirectoryURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
     let zipsDirectoryURL: URL
     let songsDirectoryURL: URL
+    @Published var busy: Bool = false
+    @Published var songDownloadsSize: String = "0.000"
+    @Published var cacheSize: String = "0.000"
     
     // MARK: BASIC INIT
     init() {
@@ -39,17 +42,75 @@ class TwibStorageManager {
         }
     }
     
-        try clearDirectory(at: songsDirectoryURL)
+    func clearCache() throws {
+        print("Clearing cache....")
+        DispatchQueue.main.async {
+            self.busy = true
+        }
+        try clearDirectory(at: tmpDirectoryURL)
+        try manager.createDirectory(at: zipsDirectoryURL, withIntermediateDirectories: true)
+        try self.syncCacheSize()
+        DispatchQueue.main.async {
+            self.busy = false
+        }
         print("Done!")
     }
     
+    func clearDownloads() throws {
+        print("Clearing downloads....")
+        DispatchQueue.main.async {
+            self.busy = true
         }
-        catch {
-            print(error.localizedDescription)
+        try clearDirectory(at: songsDirectoryURL)
+        try self.syncDownloadsSize()
+        DispatchQueue.main.async {
+            self.busy = false
+        }
+        print("Done!")
+    }
+    
+    // MARK: CALCULATING SIZE
+    func getDirectorySize(at url: URL) throws -> UInt64 {
+        var totalSize: UInt64 = 0
+        let contents = try manager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+        for fileURL in contents {
+            var isDirectory: ObjCBool = false
+            if manager.fileExists(atPath: fileURL.path, isDirectory: &isDirectory) {
+                if isDirectory.boolValue {
+                    // Recursively calculate the size of the subdirectory
+                    totalSize += try getDirectorySize(at: fileURL)
+                } else {
+                    let fileAttributes = try fileURL.resourceValues(forKeys: [.fileSizeKey])
+                    if let fileSize = fileAttributes.fileSize {
+                        totalSize += UInt64(fileSize)
+                    }
+                }
+            }
+        }
+        return totalSize
+    }
+    
+    func syncCacheSize() throws {
+        let cacheSize = try getDirectorySize(at: tmpDirectoryURL)
+        DispatchQueue.main.async {
+            let rawValue = Double(cacheSize) / (1024 * 1024)
+            self.cacheSize = String(format: "%.3f", rawValue)
         }
     }
     
+    func syncDownloadsSize() throws {
+        let downloadsSize = try getDirectorySize(at: songsDirectoryURL)
+        DispatchQueue.main.async {
+            let rawValue = Double(downloadsSize) / (1024 * 1024)
+            self.songDownloadsSize = String(format: "%.3f", rawValue)
+        }
+    }
+    
+    // MARK: HANDLE DOWNLOADS
     func handleDownload(_ incomingData: Data) -> Bool {
+        DispatchQueue.main.async {
+            self.busy = true
+        }
         let uuid = UUID().uuidString
         let fileURL = zipsDirectoryURL.appendingPathComponent(uuid + ".zip")
         do {
@@ -57,9 +118,15 @@ class TwibStorageManager {
             try Zip.unzipFile(fileURL, destination: songsDirectoryURL,
                               overwrite: true, password: nil)
             try manager.removeItem(at: fileURL)
+            DispatchQueue.main.async {
+                self.busy = false
+            }
             return true
         } catch {
             print("Error processing ZIP file: \(error.localizedDescription)")
+            DispatchQueue.main.async {
+                self.busy = false
+            }
             return false
         }
     }
