6df8ea9c62fab917881aed57ac6b7086091b8e6b
Lukas Cao <lukascao06@gmail.com>
2024-09-03 11:52:32 -0700

    Update SettingsView.swift

--- Twib Music/Views/SettingsView.swift
+++ Twib Music/Views/SettingsView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @StateObject private var storageManager = StorageManager
     var body: some View {
-        Text("No settings yet!")
+        VStack {
+            Text("Cache Size: \(storageManager.cacheSize) MB")
+                .font(.custom("Helvetica", size: 24))
+                .padding(.bottom, 6)
+            Text("Download Size: \(storageManager.songDownloadsSize) MB")
+                .font(.custom("Helvetica", size: 24))
+            Group {
+                Button("Remove Cache") {
+                    do { try StorageManager.clearCache() }
+                    catch { print("Error clearing cache: \(error)") }
+                }
+                Button("Remove ALL Downloads") {
+                    do { try StorageManager.clearDownloads() }
+                    catch { print("Error removing downloads: \(error)") }
+                }
+            }
+            .disabled(storageManager.busy)
+            .font(.custom("Helvetica", size: 18))
+            .foregroundColor(.white)
+            .padding(EdgeInsets(top: 14, leading: 28, bottom: 14, trailing: 28))
+            .background(Color.orange)
+            .cornerRadius(32)
+            .padding(.top, 24)
+        }
+        .onAppear {
+            do {
+                try StorageManager.syncCacheSize()
+                try StorageManager.syncDownloadsSize()
+            } catch {
+                print("Error syncing cache size: \(error)")
+            }
+        }
     }
 }
 
